ac20e3de874fb3b0bc39bd2257eae90afd1f5479
Flavio Auciello <flavioauciello@nebulab.it>
2018-11-24 12:32:32 +0100

    Configure Database and Gigalixir variables
    $ mix.release init
    $ gigalixir pg:create --free

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,17 @@ use Mix.Config
 config :endlesshoney, EndlesshoneyWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  server: true, # Without this line, your app will not start the web server!
+  secret_key_base: "${SECRET_KEY_BASE}",
+
+config :endlesshoney, Endlesshoney.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
 
 # Do not print debug messages in production
 config :logger, level: :info
